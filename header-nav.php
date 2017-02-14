b62f5bb26318cde4b53a70fc21b72b7c88a442dd
DESKTOP-52RPLJC\Javier Martinez <sparktechs@hotmail.com>
2017-02-14 11:54:16 -0800

    SKYWEP-22 / Se agrego estructura de navigation bar y css basico

---
+++ header-nav.php
@@ -0,0 +1,5 @@
+<!-- Configure here the content of the Header for branding and image purposes-->
+<div class="header-main">
+   <a class="header-brand" href="index"><img src="img/skyworks_logo.png"></a>
+   <span>Engineering Requests</span>
+</div> 
